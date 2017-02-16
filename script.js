2152a3b641a4ac21d68547885cb0ae515e4a1af9
Juan Cirilo <juancirilo194@yahoo.com>
2017-02-16 14:11:47 +0000

    tiny turtle

---
+++ script.js
@@ -0,0 +1,14 @@
+TinyTurtle.apply(window); 
+forward(20);
+right(90);
+forward(40);
+right(90);
+forward(20);
+right(90);
+forward(40);
+right(90);
+forward(20);
+right(40);
+forward(30);
+right(100);
+forward(30);
